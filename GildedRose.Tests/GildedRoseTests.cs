d29e16ec28aebf635e5386ab591356fae0d736f5
Nevern Govender <43511671+neverngovender@users.noreply.github.com>
2018-11-13 16:01:46 +0200

    Only added test for Sulfuras as legendary items never need to be sold and never lose quality.

--- GildedRose.Tests/GildedRoseTests.cs
+++ GildedRose.Tests/GildedRoseTests.cs
@@ -56,6 +56,7 @@ namespace GildedRose.Tests
         [TestCase("+5 Dexterity Vest", 10, 20, 9, 19)]
         [TestCase("Aged Brie", 2, 0, 1, 1)]
         [TestCase("Elixir of the Mongoose", 5, 7, 4, 6)]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
         public void GildedRoseFactoryTest(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
         {
             var item = new Item { Name = name, SellIn = sellIn, Quality = quality };
