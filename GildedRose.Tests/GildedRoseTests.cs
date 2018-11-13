344cb80923d167a3c3d14eb95e368ca585be4cb4
Nevern Govender <43511671+neverngovender@users.noreply.github.com>
2018-11-13 14:46:06 +0200

    Project setup

---
+++ GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRose.Tests
+{
+    [TestClass]
+    public class GildedRoseTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
